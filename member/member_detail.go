cb029fe8063d7b70cf4d4957c76b22b7336556f9
weiting <weiting.shi1982@gmail.com>
2021-02-20 14:50:34 +0800

    修正錯誤

--- member/member_detail.go
+++ member/member_detail.go
@@ -1,6 +1,8 @@
 package member
 
 import (
+	"go-api-sooon/app"
+	"go-api-sooon/config"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -19,6 +21,21 @@ func Do(c *gin.Context) {
 	case "loginHistory":
 		// 使用者登入紀錄
 		v, _ := c.Get("memberID")
+
+		// sql
+		db, err := config.NewDBConnect()
+		if err != nil {
+			c.JSON(http.StatusServiceUnavailable, gin.H{
+				"s":       -9, // -9系統層級 APP不顯示錯誤訊息
+				"errCode": app.DumpErrorCode(loginCodePrefix),
+				"errMsg":  err.Error(),
+			})
+			return
+		}
+		defer db.Close()
+
+		stmt, err := db.Prepare("SELECT * FROM `sooon_db`.`member_login_log` WHERE `member_id`")
+
 		c.JSON(http.StatusOK, gin.H{
 			"s": 1,
 			"c": v,
