a8f2aa1fcfed1336bfe397e784897743e339faa0
weiting <weiting.shi1982@gmail.com>
2021-02-20 15:46:29 +0800

    登入log

--- member/member_detail.go
+++ member/member_detail.go
@@ -1,6 +1,7 @@
 package member
 
 import (
+	"database/sql"
 	"go-api-sooon/app"
 	"go-api-sooon/config"
 	"net/http"
@@ -20,7 +21,7 @@ func Do(c *gin.Context) {
 		return
 	case "loginHistory":
 		// 使用者登入紀錄
-		v, _ := c.Get("memberID")
+		memberID, _ := c.Get("memberID")
 
 		// sql
 		db, err := config.NewDBConnect()
@@ -34,11 +35,29 @@ func Do(c *gin.Context) {
 		}
 		defer db.Close()
 
-		stmt, err := db.Prepare("SELECT * FROM `sooon_db`.`member_login_log` WHERE `member_id`")
+		stmt, err := db.Prepare("SELECT * FROM `sooon_db`.`member_login_log` WHERE `member_id` = ?")
+		if err != nil {
+			c.JSON(http.StatusServiceUnavailable, gin.H{
+				"s":       -9, // -9系統層級 APP不顯示錯誤訊息
+				"errCode": app.DumpErrorCode(loginCodePrefix),
+				"errMsg":  err.Error(),
+			})
+			return
+		}
+
+		rows, err := stmt.Query(memberID)
+		if err != nil && err != sql.ErrNoRows {
+			c.JSON(http.StatusServiceUnavailable, gin.H{
+				"s":       -9, // -9系統層級 APP不顯示錯誤訊息
+				"errCode": app.DumpErrorCode(loginCodePrefix),
+				"errMsg":  err.Error(),
+			})
+			return
+		}
 
 		c.JSON(http.StatusOK, gin.H{
 			"s": 1,
-			"c": v,
+			"c": rows,
 		})
 		return
 	}
